786d72529186343b534bbdf7968d99e20bf0fa60
harshitdhodi <harshit.dhodi2108@gmail.com>
2025-04-18 18:01:45 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -329,7 +329,6 @@ setInterval(() => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
 const port = process.env.PORT || 3006;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
