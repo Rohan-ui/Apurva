74e2d318b7c06c2d344571a0f8c83c9787e562b7
harshitdhodi <harshit.dhodi2108@gmail.com>
2025-04-19 12:17:46 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -332,4 +332,4 @@ const port = process.env.PORT || 3006;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   // generateAllSitemaps(); // Generate sitemaps on server start
-});
+}); 
